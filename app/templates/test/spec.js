b5b9f8e0801dde48307c8d57b0c00a8bd272f8e0
metaraine <raineorshine@gmail.com>
2014-12-09 16:35:54 -0700

    Remove commas

--- app/templates/test/spec.js
+++ app/templates/test/spec.js
@@ -1,7 +1,7 @@
-'use strict';
-var assert = require('insist');
-var <%=camelize(props.project)%> = require('./index.js');
+'use strict'
+var assert = require('insist')
+var <%=camelize(props.project)%> = require('./index.js')
 
 it('should do something', function () {
-	assert.strictEqual(true, false);
-});
+	assert.strictEqual(true, false)
+})
