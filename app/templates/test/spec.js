34db12e899efe95de6caabeb7fd94edceb135ca0
metaraine <raineorshine@gmail.com>
2014-12-09 16:42:21 -0700

    Reference correct index.js now that we are in test/spec.js

--- app/templates/test/spec.js
+++ app/templates/test/spec.js
@@ -1,6 +1,6 @@
 'use strict'
 var assert = require('insist')
-var <%=camelize(props.project)%> = require('./index.js')
+var <%=camelize(props.project)%> = require('../index.js')
 
 it('should do something', function () {
 	assert.strictEqual(true, false)
